a62f65e995ad4cefd4b3f08a526641ee25c441ba
Franziskhaner <fco.jesus.diaz@hotmail.com>
2018-05-05 14:42:03 +0200

    implementada relacion ciudad-pais
    establecida relacion uno a muchos desde la tabla país a la de ciudad

---
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/cities/City.java
@@ -0,0 +1,62 @@
+package es.uca.iw.rentAndDream.cities;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import es.uca.iw.rentAndDream.countries.Country;
+
+@Entity
+public class City {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String name;
+	
+	private String province;
+	
+	private Integer postalCode;
+	
+	protected City() {
+	}
+
+	public City(String name, String province, Integer postalCode) {
+		this.name = name;
+		this.province = province;
+		this.postalCode = postalCode;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	} 
+	
+	public Integer getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(Integer postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	@ManyToOne(fetch=FetchType.LAZY) 
+    public Country country;
+}
