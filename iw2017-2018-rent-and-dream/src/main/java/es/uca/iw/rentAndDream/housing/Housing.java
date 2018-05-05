57108d165ecc4717e146c2aad2a85931ca45d8ca
tonixiclana <tonixiclana@hotmail.com>
2018-05-05 18:43:04 +0200

    Mas cosas subidas

---
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/housing/Housing.java
@@ -0,0 +1,107 @@
+package es.uca.iw.rentAndDream.housing;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import es.uca.iw.rentAndDream.users.User;
+
+@Entity
+public class Housing {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+    @ManyToOne(fetch=FetchType.LAZY)
+	private User user;
+	
+	private String name;
+	
+	private Float assessment;
+	
+	private String description;
+	
+	private Integer bedrooms;
+	
+	private Integer beds;
+	
+	private Boolean airConditioner;
+	
+	protected Housing(){}
+	
+	public Housing(String name, Float assessment, String description,
+			Integer bedrooms, Integer beds, Boolean airConditioner) {
+		this.name = name;
+		this.assessment = assessment;
+		this.description = description;
+		this.bedrooms = bedrooms;
+		this.beds = beds;
+		this.airConditioner = airConditioner;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public User getIdUser() {
+		return user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getAssessment() {
+		return assessment;
+	}
+
+	public void setAssessment(Float assessment) {
+		this.assessment = assessment;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getBedrooms() {
+		return bedrooms;
+	}
+
+	public void setBedrooms(Integer bedrooms) {
+		this.bedrooms = bedrooms;
+	}
+
+	public Integer getBeds() {
+		return beds;
+	}
+
+	public void setBeds(Integer beds) {
+		this.beds = beds;
+	}
+
+	public Boolean getAirConditioner() {
+		return airConditioner;
+	}
+
+	public void setAirConditioner(Boolean airConditioner) {
+		this.airConditioner = airConditioner;
+	}
+
+	@Override
+	public String toString() {
+		return "Housing [id=" + id + ", user=" + user + ", name=" + name + ", assessment="
+				+ assessment + ", description=" + description + ", bedrooms=" + bedrooms + ", beds=" + beds
+				+ ", airConditioner=" + airConditioner + "]";
+	}
+}
---
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/housing/HousingEditor.java
@@ -0,0 +1,137 @@
+package es.uca.iw.rentAndDream.housing;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.data.Binder;
+import com.vaadin.data.converter.StringToBooleanConverter;
+import com.vaadin.data.converter.StringToFloatConverter;
+import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.spring.annotation.SpringComponent;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
+
+import es.uca.iw.rentAndDream.security.SecurityUtils;
+import es.uca.iw.rentAndDream.users.RoleType;
+
+@SpringComponent
+@UIScope
+public class HousingEditor extends VerticalLayout {
+	
+	private final HousingService service;
+	
+	/**
+	 * The currently edited user
+	 */
+	private Housing housing;
+
+	private Binder<Housing> binder = new Binder<>(Housing.class);
+	
+	/* Fields to edit properties in Housing entity */
+	TextField name = new TextField("Name");
+	TextField assessment = new TextField("Calification");
+	TextField description = new TextField("Description");
+	TextField bedrooms = new TextField("Bedrooms");
+	TextField beds = new TextField("Beds");
+	TextField airConditioner = new TextField("airConditioner");
+
+	/* Action buttons */
+	Button save = new Button("Save", FontAwesome.SAVE);
+	Button cancel = new Button("Cancel");
+	Button delete = new Button("Delete", FontAwesome.TRASH_O);
+	
+	/* Layout for buttons */
+	CssLayout actions = new CssLayout(save, cancel, delete);
+
+
+	@Autowired
+	public HousingEditor(HousingService service) {
+		this.service = service;
+
+		addComponents(name, assessment, description, bedrooms, beds, airConditioner, actions);
+
+		binder.forField(assessment)
+			.withConverter(
+				new StringToFloatConverter("Must enter a number"))
+		  	.bind(Housing::getAssessment, Housing::setAssessment);
+		
+		binder.forField(bedrooms)
+		.withConverter(
+			new StringToIntegerConverter("Must enter a number"))
+	  	.bind(Housing::getBedrooms, Housing::setBedrooms);
+		
+		binder.forField(beds)
+		.withConverter(
+			new StringToIntegerConverter("Must enter a number"))
+	  	.bind(Housing::getBeds, Housing::setBeds);
+		
+		binder.forField(airConditioner)
+		.withConverter(
+			new StringToBooleanConverter("Must enter a Bool"))
+	  	.bind(Housing::getAirConditioner, Housing::setAirConditioner);
+		
+		// bind using naming convention
+		binder.bindInstanceFields(this);
+		
+		// Configure and style components
+		setSpacing(true);
+		actions.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+		save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+		save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+
+		// wire action buttons to save, delete and reset
+		save.addClickListener(e -> service.save(housing));
+		delete.addClickListener(e -> service.delete(housing));
+		cancel.addClickListener(e -> editHousing(housing));
+		setVisible(false);
+		
+		// Solo borra el admin
+		delete.setEnabled(SecurityUtils.hasRole(RoleType.ADMIN));
+	}
+
+	public interface ChangeHandler {
+
+		void onChange();
+	}
+
+	public final void editHousing(Housing c) {
+		if (c == null) {
+			setVisible(false);
+			return;
+		}
+		final boolean persisted = c.getId() != null;
+		if (persisted) {
+			// Find fresh entity for editing
+			housing = service.findOne(c.getId());
+		}
+		else {
+			housing = c;
+		}
+		cancel.setVisible(persisted);
+
+		// Bind user properties to similarly named fields
+		// Could also use annotation or "manual binding" or programmatically
+		// moving values from fields to entities before saving
+		binder.setBean(housing);
+
+		setVisible(true);
+
+		// A hack to ensure the whole form is visible
+		save.focus();
+		// Select all text in firstName field automatically
+		name.selectAll();
+	}
+
+	public void setChangeHandler(ChangeHandler h) {
+		// ChangeHandler is notified when either save or delete
+		// is clicked
+		save.addClickListener(e -> h.onChange());
+		delete.addClickListener(e -> h.onChange());
+	}
+
+}
