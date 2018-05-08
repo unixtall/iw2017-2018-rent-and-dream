be60f83f583d68faf6a9100f1976dc18126f1fbe
Ismael Nieto <indc94@gmail.com>
2018-05-08 16:56:00 +0200

    Rename Birth Date to Birthday in User

--- iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/users/User.java
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/users/User.java
@@ -36,7 +36,7 @@ public class User implements UserDetails{
 
 	private String password;
 	
-	private LocalDate birthDate;
+	private LocalDate birthday;
 	
 	private String dni;
 	
@@ -55,12 +55,12 @@ public class User implements UserDetails{
 	protected User() {
 	}
 
-	public User(String firstName, String lastName, String username, String email, LocalDate birthDate, String dni, String telephone, RoleType role) {
+	public User(String firstName, String lastName, String username, String email, LocalDate birthday, String dni, String telephone, RoleType role) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.username = username;
 		this.email = email;
-		this.birthDate = birthDate;
+		this.birthday = birthday;
 		this.dni = dni;
 		this.telephone = telephone;
 		this.role = role;
@@ -105,12 +105,12 @@ public class User implements UserDetails{
 		this.email = email;
 	}
 
-	public LocalDate getBirthDate() {
-		return birthDate;
+	public LocalDate getBirthday() {
+		return birthday;
 	}
 
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
+	public void setBirthDate(LocalDate birthday) {
+		this.birthday = birthday;
 	}
 
 	public String getDni() {
