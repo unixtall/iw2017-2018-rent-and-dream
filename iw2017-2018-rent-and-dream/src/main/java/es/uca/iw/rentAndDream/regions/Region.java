890f8dfd75366866db6a4081433fc20d63e01677
Franziskhaner <fco.jesus.diaz@hotmail.com>
2018-05-12 16:16:15 +0200

    Housing search con filtro de ciudades OK

--- iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/regions/Region.java
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/regions/Region.java
@@ -1,11 +1,16 @@
 package es.uca.iw.rentAndDream.regions;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+import es.uca.iw.rentAndDream.cities.City;
 import es.uca.iw.rentAndDream.countries.Country;
 
 @Entity
@@ -16,6 +21,9 @@ public class Region {
 	private Long id;	
 	private String name;	
 	private String code;	
+	
+	@OneToMany(fetch=FetchType.LAZY, cascade = CascadeType.ALL, mappedBy="region")
+    public List<City> cities;
 	
     @ManyToOne(fetch=FetchType.LAZY)
     private Country country;
@@ -53,6 +61,14 @@ public class Region {
 
 	public void setCountry(Country country) {
 		this.country = country;
+	}
+	
+	public List<City> getCities() {
+		return cities;
+	}
+	
+	public void setCities(List<City> cities) {
+		this.cities = cities;
 	}
 	
 	@Override
--- iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/regions/RegionRepository.java
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/regions/RegionRepository.java
@@ -2,12 +2,19 @@ package es.uca.iw.rentAndDream.regions;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import es.uca.iw.rentAndDream.cities.City;
+import es.uca.iw.rentAndDream.countries.Country;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
 	public List<Region> findByNameStartsWithIgnoreCase(String name);
+	
+	@EntityGraph(attributePaths = {"cities"})
+	public Region findOne(Long id);
 	
 	public Region findByName(String name);
 }
