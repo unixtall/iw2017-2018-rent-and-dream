2892e23f5900a63b6739096c0671ba8b3a0b89ed
Ismael Nieto <indc94@gmail.com>
2018-05-17 16:23:44 +0200

    Validators en UserScreenRegister

--- iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/users/UserRegisterEditor.java
+++ iw2017-2018-rent-and-dream/src/main/java/es/uca/iw/rentAndDream/users/UserRegisterEditor.java
@@ -1,11 +1,20 @@
 package es.uca.iw.rentAndDream.users;
 
+import java.time.LocalDate;
 import java.util.EnumSet;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.vaadin.data.Binder;
+import com.vaadin.data.BinderValidationStatus;
+import com.vaadin.data.Validator;
 import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.data.validator.DateRangeValidator;
+import com.vaadin.data.validator.EmailValidator;
+import com.vaadin.data.validator.StringLengthValidator;
+import com.vaadin.event.SerializableEventListener;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.spring.annotation.SpringComponent;
@@ -14,6 +23,9 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.DateField;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
@@ -22,7 +34,7 @@ import es.uca.iw.rentAndDream.security.SecurityUtils;
 
 @SpringComponent
 @UIScope
-public class UserRegisterEditor extends VerticalLayout {
+public class UserRegisterEditor extends FormLayout {
 	
 	private final UserService service;
 	
@@ -37,7 +49,8 @@ public class UserRegisterEditor extends VerticalLayout {
 	TextField firstName = new TextField("First name");
 	TextField lastName = new TextField("Last name");
 	TextField username = new TextField("Username");
-	TextField password = new TextField("Password");
+	PasswordField passwordField = new PasswordField("Password");
+	PasswordField confirmPasswordField = new PasswordField("Confirm Password");
 	TextField email = new TextField("Email");
 	DateField birthday = new DateField("Birthday");
 	TextField dni = new TextField("Dni");
@@ -51,16 +64,82 @@ public class UserRegisterEditor extends VerticalLayout {
 
 
 	@Autowired
-	public UserRegisterEditor(UserService service) {
+	public UserRegisterEditor(UserService service){
 		this.service = service;
 
-		addComponents(firstName, lastName, username, password, email, birthday, dni, telephone, actions);
+		addComponents(firstName, lastName, username, passwordField, confirmPasswordField, email, birthday, dni, telephone, actions);
+		
+		//Binders
+		
+		binder.forField(firstName)
+        	.asRequired("First name may not be empty")
+        	.withValidator(
+        		    name -> name.length() >= 2,
+        		    "First name must contain at least two characters")
+        	.bind(User::getFirstName, User::setFirstName);
+		
+		binder.forField(lastName)
+    		.asRequired("Last name may not be empty")
+    		.withValidator(
+        		    name -> name.length() >= 2,
+        		    "Last name must contain at least two characters")
+    		.bind(User::getLastName, User::setLastName);
+		
+		binder.forField(username)
+    		.asRequired("Username may not be empty")
+    		.withValidator(new StringLengthValidator(
+    		        "Username must be between 3 and 15 characters long",
+    		        3, 15))
+    		.bind(User::getUsername, User::setUsername);
+		
+		binder.forField(passwordField)
+        	.asRequired("Password may not be empty")
+        	.withValidator(new StringLengthValidator(
+        			"Password must be at least 7 characters long", 7, null))
+        	.bind(User::getPassword, User::setPassword);
+
+		binder.forField(confirmPasswordField)
+        	.asRequired("Must confirm password")
+        	.bind(User::getPassword, (person, password) -> {});
+
+		binder.forField(email)
+		        .asRequired("Email may not be empty")
+		        .withValidator(new EmailValidator("Not a valid email address"))
+		        .bind(User::getEmail, User::setEmail);
+		
+		binder.forField(birthday)
+			.asRequired("Birthday may not be empty")
+			.withValidator(new DateRangeValidator("You must be more than 18 years old", null, LocalDate.now().minusYears(18)))
+			.bind(User::getBirthday, User::setBirthday);
+		
+		binder.forField(dni)
+    		.asRequired("DNI name may not be empty")
+    		.bind(User::getDni, User::setDni);
+		
 		binder.forField(telephone)
 			.withConverter(new StringToIntegerConverter("Must enter a number"))
 			.bind(User::getTelephone, User::setTelephone);
-		// bind using naming convention
-		binder.bindInstanceFields(this);
-	
+		
+		binder.withValidator(Validator.from(user -> {
+		    if (passwordField.isEmpty() || confirmPasswordField.isEmpty()) {
+		        return true;
+		    } else {
+		        return Objects.equals(passwordField.getValue(),
+		                confirmPasswordField.getValue());
+		    }
+		}, "Entered password and confirmation password must match"));
+		
+		Label validationStatus = new Label();
+		binder.setStatusLabel(validationStatus);
+		
+		//binder.getValidationStatusHandler();
+
+		//binder.bindInstanceFields(this);
+		
+		binder.setBean(user);
+ 
+        save.setEnabled(false);
+        
 		// Configure and style components
 		setSpacing(true);
 		actions.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
@@ -68,8 +147,14 @@ public class UserRegisterEditor extends VerticalLayout {
 		save.setClickShortcut(ShortcutAction.KeyCode.ENTER);
 
 		// wire action buttons to save
-		save.addClickListener(e -> service.save(user));
-		setVisible(false);
+		//save.addClickListener(e -> service.save(user));
+		//setVisible(false);
+		
+		save.addClickListener(
+	              event -> service.save(binder.getBean()));
+	 
+	        binder.addStatusChangeListener(
+	                event -> save.setEnabled(binder.isValid()));
 	
 	}
 
